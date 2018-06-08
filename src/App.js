31c6e2612c5a914eccb4b7e27f000b86cd54e805
Andreas Pätzold <andi.paetzold@gmail.com>
2018-06-08 21:26:21 +0200

    Added .editorconfig

--- src/App.js
+++ src/App.js
@@ -4,63 +4,63 @@ import NewEntryCard from './NewEntryCard';
 import { ENTRIES } from './data';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { entries: ENTRIES, };
-  }
+        this.state = { entries: ENTRIES, };
+    }
 
-  setEntryDone(entry) {
-    this.updateEntryDone(entry, true);
-  }
+    setEntryDone(entry) {
+        this.updateEntryDone(entry, true);
+    }
 
-  setEntryNotDone(entry) {
-    this.updateEntryDone(entry, false);
-  }
+    setEntryNotDone(entry) {
+        this.updateEntryDone(entry, false);
+    }
 
-  addEntry(name) {
-    this.setState(prevState => {
-      const entry = {
-        id: Math.max(...prevState.entries.map(e => e.id)) + 1,
-        name,
-        done: false,
-      };
+    addEntry(name) {
+        this.setState(prevState => {
+            const entry = {
+                id: Math.max(...prevState.entries.map(e => e.id)) + 1,
+                name,
+                done: false,
+            };
 
-      return {
-        entries: [
-          ...prevState.entries,
-          entry
-        ]
-      };
-    });
-  }
+            return {
+                entries: [
+                    ...prevState.entries,
+                    entry
+                ]
+            };
+        });
+    }
 
-  updateEntryDone(entry, done) {
-    this.setState(prevState => {
-      const entries = prevState.entries;
-      const entryIndex = entries.findIndex(e => e.id === entry.id);
+    updateEntryDone(entry, done) {
+        this.setState(prevState => {
+            const entries = prevState.entries;
+            const entryIndex = entries.findIndex(e => e.id === entry.id);
 
-      if (entryIndex < 0) {
-        return {};
-      }
+            if (entryIndex < 0) {
+                return {};
+            }
 
-      entries[entryIndex].done = done;
-      return { entries };
-    });
-  }
+            entries[entryIndex].done = done;
+            return { entries };
+        });
+    }
 
-  render() {
-    const doneEntries = this.state.entries.filter(e => e.done);
-    const notDoneEntries = this.state.entries.filter(e => !e.done);
+    render() {
+        const doneEntries = this.state.entries.filter(e => e.done);
+        const notDoneEntries = this.state.entries.filter(e => !e.done);
 
-    return (
-      <div className="container">
-        <NewEntryCard addEntry={this.addEntry.bind(this)} />
-        <EntryListCard entries={notDoneEntries} entryClick={this.setEntryDone.bind(this)} />
-        <EntryListCard title="Done" entries={doneEntries} entryClick={this.setEntryNotDone.bind(this)} />
-      </div>
-    );
-  }
+        return (
+            <div className="container">
+                <NewEntryCard addEntry={this.addEntry.bind(this)} />
+                <EntryListCard entries={notDoneEntries} entryClick={this.setEntryDone.bind(this)} />
+                <EntryListCard title="Done" entries={doneEntries} entryClick={this.setEntryNotDone.bind(this)} />
+            </div>
+        );
+    }
 }
 
 export default App;
